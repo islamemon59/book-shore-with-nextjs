c606f55519393ea99a15a3c3122b212959700c83
islamemon59 <islamemon59@gmail.com>
2025-09-06 04:13:30 +0600

    delete method added

--- src/app/api/books/[id]/route.js
+++ src/app/api/books/[id]/route.js
@@ -1,6 +1,8 @@
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-import { ObjectId } from 'mongodb';
+import { ObjectId } from "mongodb";
 import { collectionObj, dbConnect } from "@/lib/dbConnect";
+import { authOptions } from "@/lib/authOptions";
 
 export async function GET(request, { params }) {
   try {
@@ -24,4 +26,39 @@ export async function GET(request, { params }) {
       { status: 500 }
     );
   }
+}
+
+
+export async function DELETE(req, { params }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  if (session.user.role !== "admin") {
+    return NextResponse.json(
+      { error: "Forbidden: Admins only" },
+      { status: 403 }
+    );
+  }
+
+  const booksCollection = await dbConnect(collectionObj.booksCollection);
+
+  const { id } = params;
+
+  if (!ObjectId.isValid(id)) {
+    return NextResponse.json({ error: "Invalid book ID" }, { status: 400 });
+  }
+
+  const result = await booksCollection.deleteOne({ _id: new ObjectId(id) });
+
+  if (result.deletedCount === 0) {
+    return NextResponse.json({ error: "Book not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({
+    success: true,
+    message: "Book deleted successfully",
+  });
 }
