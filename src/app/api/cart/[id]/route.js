f526fc0f9c37326567525a9e20dd67ce23f8fd95
islamemon59 <islamemon59@gmail.com>
2025-09-09 20:47:30 +0600

    update quantity api added

---
+++ src/app/api/cart/[id]/route.js
@@ -0,0 +1,36 @@
+import { authOptions } from "@/lib/authOptions";
+import { collectionObj, dbConnect } from "@/lib/dbConnect";
+import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function PATCH(req, { params }) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { id } = await params;
+  const { change } = await req.json();
+  try {
+    const cartDataCollection = await dbConnect(
+      collectionObj.cartDataCollection
+    );
+    const result = await cartDataCollection.findOneAndUpdate(
+      { _id: new ObjectId(id), userEmail: session?.user?.email },
+      { $inc: { quantity: change } },
+      { returnDocument: "after" }
+    );
+    if (result.value) {
+      return NextResponse.json({ error: "Item not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ remove: true });
+  } catch (error) {
+    console.error("Cart update error:", err);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
