eeba2757c6076fb0a7f98db2f501693b073eaf17
islamemon59 <islamemon59@gmail.com>
2025-09-19 17:46:03 +0600

    loader updated

---
+++ src/app/Components/BookLoader/BookLoader.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { FaBook } from "react-icons/fa";
+
+const BookLoader = () => {
+  return (
+    <FaBook
+      className="text-[3rem] animate-rotate-reverse text-primary"
+    />
+  );
+};
+
+export default BookLoader;
--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import { Suspense } from "react";
+import BookLoader from "./Components/BookLoader/BookLoader";
 
 export default function Loading() {
   return (
     <div className="hero min-h-screen" style={{ backgroundColor: "#EDF6F8" }}>
       <div className="hero-content text-center flex flex-col items-center">
-        <span
-          className="loading loading-spinner loading-lg mb-4"
-          style={{ color: "#3489BD" }}
-        ></span>
-        <p className="text-xl font-semibold" style={{ color: "#144D75" }}>
+        {/* Animated Book Loader */}
+        <Suspense fallback={null}>
+          <BookLoader />
+        </Suspense>
+
+        <p className="text-xl font-semibold mt-4 text-primary">
           Loading, please wait...
         </p>
+
         <progress
-          className="progress w-56 mt-4"
-          style={{ accentColor: "#2E7A7A" }}
+          className="progress w-56 mt-4 bg-base-100 text-accent"
         ></progress>
       </div>
     </div>
